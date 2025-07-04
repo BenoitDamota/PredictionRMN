576db914daf6aa77c106eab06ff9a68fefd1dfd4
KreeZeG123 <manfaloti.yamis@gmail.com>
2025-07-04 11:23:45 +0200

    chores(main-app): Preparing for remote use

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ from app.routes import main_routes
 from app.api import api_routes
 
 
-def create_app():
+def create_app(serve_frontend=True):
     # Create a flask instance
     app = Flask(__name__, static_folder=Config.STATIC_FOLDER)
 
@@ -15,7 +15,8 @@ def create_app():
     CORS(app, origins=app.config["CORS_ORIGINS"])
 
     # Handle blueprints
-    app.register_blueprint(main_routes)
+    if serve_frontend:
+        app.register_blueprint(main_routes)
     app.register_blueprint(api_routes, url_prefix="/api")
 
     return app
